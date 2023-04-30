f6c220c7cdd1f86bbc63ea7296c668898b49e0b9
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-30 10:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 let images = [
-	'https://tse1.mm.bing.net/th?id=OIP._4MayEo24uOqL16nY5gp0gHaE8&pid=Api&P=0',
-	'https://tse2.mm.bing.net/th?id=OIP.jIGSidW76D_vlYcaeSCbVwHaEo&pid=Api&P=0',
-	'https://tse1.mm.bing.net/th?id=OIP.RYDJeNBXkSnl4zsmaLSwTQHaEZ&pid=Api&P=0'
+	'https://picsum.photos/id/237/200/300',
+	'https://picsum.photos/id/238/200/300',
+	'https://picsum.photos/id/239/200/300'
 ]; //urls of images
 
 document.getElementById('download-images-button').addEventListener('click',(event)=>{
@@ -11,17 +11,17 @@ document.getElementById('download-images-button').addEventListener('click',(even
 		return new Promise((resolve,reject) => {
 			let img = new Image();//getting image 
 
-			img.onload = () => resolve(img);
-			img.onerror = () => reject(`Failed to load image's URL: ${url}`);
-			img.src = url;
+			img.onload = () => resolve(img); //upon laoding the image the promise is resolved
+			img.onerror = () => reject(`Failed to load image's URL: ${url}`); //if error give the statement
+			img.src = url; //save the image source
 	});
 	});
 
 	Promise.all(promises).then(images => {
 		images.forEach((image) => {
-			document.getElementById('output').appendChild(image);
+			document.getElementById('output').appendChild(image); //append the image tag as and when the promise is resolved
 		});
 	}).catch(error => {
-		console.log(`there is an ${error}`);
+		console.log(error);
 	});
 });
