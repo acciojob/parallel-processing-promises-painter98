59ad1d2c772f8e3cd936412183fb13e8d18f9de3
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-30 10:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let images = [
 	'https://tse1.mm.bing.net/th?id=OIP._4MayEo24uOqL16nY5gp0gHaE8&pid=Api&P=0',
 	'https://tse2.mm.bing.net/th?id=OIP.jIGSidW76D_vlYcaeSCbVwHaEo&pid=Api&P=0',
 	'https://tse1.mm.bing.net/th?id=OIP.RYDJeNBXkSnl4zsmaLSwTQHaEZ&pid=Api&P=0'
-];
+]; //urls of images
 
 document.getElementById('download-images-button').addEventListener('click',(event)=>{
 
-	let promises = images.map((url)=>{
+	let promises = images.map((url)=>{ //creating promise for each of the image url
 		return new Promise((resolve,reject) => {
-			let image = new Image();
+			let img = new Image();//getting image 
 
-			image.onload = () => resolve(image);
-			image.onerror = () => reject(`Failed to load image's URL: ${url}`);
-			image.src = url;
+			img.onload = () => resolve(img);
+			img.onerror = () => reject(`Failed to load image's URL: ${url}`);
+			img.src = url;
 	});
 	});
 
